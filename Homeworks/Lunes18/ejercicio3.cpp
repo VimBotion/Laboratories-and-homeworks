23ac027c65e527203ab92a5aec3bf6e9bc90d865
esteban <sucoandchino@gmail.com>
2023-12-17 18:22:31 -0500

    Functions that inversely order the rows and columns of the matrix, respectively

--- Homeworks/Lunes18/ejercicio3.cpp
+++ Homeworks/Lunes18/ejercicio3.cpp
@@ -1,26 +1,34 @@
 // ejercicio 34 de la pagina 170
 #include <iostream>
+#include <stdlib.h>
 
 void ingresarNumeros(int** matriz, int filas, int columnas);
 void mostrarMatriz(int **matriz, int filas, int columnas);
+void virasFilas(int **matriz, int filas, int columnas);
+void virarColumnas(int **matriz, int filas, int columnas);
 
 int main(){
-    int rows, cols;
+    int filas, columnas;
 
     std::cout << "Ingrese el numero de filas de la matriz: ";
-    std::cin >> rows;
+    std::cin >> filas;
     std::cout << "Ingrese el numero de columnas de la matriz: ";
-    std::cin >> cols;
+    std::cin >> columnas;
 
-    int **matriz = new int*[rows];
-    for (int i = 0; i < rows; i++){
-        matriz[i] = new int[cols];
+    int **matriz = new int*[filas];
+    for (int i = 0; i < filas; i++){
+        matriz[i] = new int[columnas];
     }
 
-    ingresarNumeros(matriz, rows, cols);
-    mostrarMatriz(matriz, rows, cols);
+    ingresarNumeros(matriz, filas, columnas);
+    system("clear");
+    std::cout << "La matriz con las filas viradas es: \n";
+    virasFilas(matriz, filas, columnas);
 
-    for (int i = 0; i < rows; i++){
+    std::cout << "La matriz con las columnas viradas es: \n";
+    virarColumnas(matriz, filas, columnas);
+
+    for (int i = 0; i < filas; i++){
         delete[] matriz[i];
     }
 
@@ -39,6 +47,24 @@ void ingresarNumeros(int** matriz, int filas, int columnas){
 void mostrarMatriz(int **matriz, int filas, int columnas){
     for (int i = 0; i < filas; i++){
         for (int j = 0; j < columnas; j++){
+            std::cout << matriz[i][j] << '\t';
+        }
+        std::cout << '\n';
+    }
+}
+
+void virasFilas(int **matriz, int filas, int columnas){
+    for (int i = filas - 1; i >= 0; i--){
+        for (int j = 0; j < columnas; j++){
+            std::cout << matriz[i][j] << '\t';
+        }
+        std::cout << '\n';
+    }
+}
+
+void virarColumnas(int **matriz, int filas, int columnas){
+    for (int i = 0; i < filas; i++){
+        for (int j = columnas - 1; j >= 0; j--){
             std::cout << matriz[i][j] << '\t';
         }
         std::cout << '\n';
