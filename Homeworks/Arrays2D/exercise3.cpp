6e0b7886544e1e8bb6c816a2ca3e808896738e37
MrRobotAE10 <sucoandchino@gmail.com>
2024-01-14 11:14:50 -0500

    Changed directory names

---
+++ Homeworks/Arrays2D/exercise3.cpp
@@ -0,0 +1,66 @@
+// Ejercicio 35 de la pagina 170
+#include <iostream>
+
+void ingresarNumeros(int **matriz, int tamano);
+void rotarMatrizNoventa(int **matriz, int tamano);
+void dibujarArreglo(int **matriz, int tamano);
+
+int main(){
+    int tamano;
+    std::cout << "Ingrese el orden de la matriz: ";
+    std::cin >> tamano;
+
+    int **matriz = new int*[tamano];
+    for (int i = 0; i < tamano; i++){
+        matriz[i] = new int[tamano];
+    }
+
+    ingresarNumeros(matriz, tamano);
+    std::cout << '\n';
+    dibujarArreglo(matriz, tamano);
+    std::cout << 'La matriz rotada noventa grados\n';
+    rotarMatrizNoventa(matriz, tamano);
+
+    for(int i = 0; i < tamano; i++){
+        delete[] matriz[i];
+    }
+    
+    delete[] matriz;
+}
+
+void ingresarNumeros(int **matriz, int tamano){
+    for (int i = 0; i < tamano; i++){
+        for (int j = 0; j < tamano; j++){
+            std::cout << "Ingrese un numero en la fila " << i + 1 << " y la columna " << j + 1 << '\n';
+            std::cin >> matriz[i][j];
+        }
+    }
+}
+void dibujarArreglo(int **matriz, int tamano){
+    for(int i = 0; i < tamano; i++){
+        for (int j = 0; j < tamano; j++){
+            std::cout << matriz[i][j] << '\t';
+        }
+        std::cout << '\n';
+    }
+}
+
+void rotarMatrizNoventa(int **matriz, int tamano){
+    int **matrizRotada = new int*[tamano];
+    for (int i = 0; i < tamano; i++){
+        matrizRotada[i] = new int[tamano];
+    }
+
+    for (int i = 0; i < tamano; i++){
+        for (int j = 0; j < tamano; j++){
+            matrizRotada[j][tamano - 1 - i] = matriz[i][j];
+        }
+    }
+
+    dibujarArreglo(matrizRotada, tamano);
+
+    for(int i = 0; i < tamano; i++){
+        delete[] matrizRotada[i];
+    }
+    delete[] matrizRotada;
+}
