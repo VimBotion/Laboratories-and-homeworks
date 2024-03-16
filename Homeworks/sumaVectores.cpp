6dfbbabec4d5b7c196566d1dfd5a7a9aacab940e
MrRobotAE10 <sucoandchino@gmail.com>
2024-03-15 22:18:38 -0500

    Feature sumarVectores

--- Homeworks/sumaVectores.cpp
+++ Homeworks/sumaVectores.cpp
@@ -16,6 +16,12 @@ public:
     void calcularModulo() {
         modulo = std::sqrt(componenteX * componenteX + componenteY * componenteY);
     }
+
+    Vector sumarVectores(const Vector& otroVector) {
+        double sumaX = componenteX + otroVector.componenteX;
+        double sumaY = componenteY + otroVector.componenteY;
+        return Vector(sumaX, sumaY);
+    }
 };
 
 Vector pedirComponentes(char vectorName){
@@ -35,6 +41,8 @@ int main() {
     std::cout << "Modulo del vectorA: " << vectorA.modulo << std::endl;
     std::cout << "Modulo del vectorB: " << vectorB.modulo << std::endl;
 
+    Vector vectorResultante = vectorA.sumarVectores(vectorB);
+    std::cout << "El modulo de la suma de vectorA y vectorB: " << vectorResultante.modulo << std::endl;
+
     return 0;
 }
-
