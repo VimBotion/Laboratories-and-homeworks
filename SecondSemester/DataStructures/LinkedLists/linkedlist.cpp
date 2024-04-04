78f807607f5a4c29c7c4804fc437bd57526deccc
MrRobotAE10 <sucoandchino@gmail.com>
2024-04-04 07:27:34 -0500

    change to spanish

--- SecondSemester/DataStructures/LinkedLists/linkedlist.cpp
+++ SecondSemester/DataStructures/LinkedLists/linkedlist.cpp
@@ -1,33 +1,30 @@
 #include <iostream>
 
-// Define a struct for the nodes of the linked list
-struct Node {
-    int data;
-    Node* next;
+struct Nodo {
+    int dato;
+    Nodo* next;
 };
 
-// Function to insert a node at the end of the linked list
-void insert(Node*& head, int data) {
-    Node* newNode = (Node *)malloc(sizeof(Node));
-    newNode->data = data;
-    newNode->next = nullptr;
+void insertar(Nodo*& head, int dato) {
+    Nodo* nuevo = (Nodo *)malloc(sizeof(Nodo));
+    nuevo->dato = dato;
+    nuevo->next = nullptr;
 
     if (head == nullptr) {
-        head = newNode;
+        head = nuevo;
     } else {
-        Node* temp = head;
+        Nodo* temp = head;
         while (temp->next != nullptr) {
             temp = temp->next;
         }
-        temp->next = newNode;
+        temp->next = nuevo;
     }
 }
 
-// Function to print the linked list
-void printList(Node* head) {
-    Node* temp = head;
+void imprimir(Nodo* head) {
+    Nodo* temp = head;
     while (temp != nullptr) {
-        std::cout << temp->data << " ";
+        std::cout << temp->dato << " ";
         std::cout << "Siguiente direccion " << temp->next << " ";
         std::cout << '\n';
         temp = temp->next;
@@ -35,31 +32,16 @@ void printList(Node* head) {
     std::cout << std::endl;
 }
 
-// Function to delete the entire linked list
-void deleteList(Node*& head) {
-    while (head != nullptr) {
-        Node* temp = head;
-        head = head->next;
-        delete temp;
-    }
-}
-
 int main() {
-    Node* head = nullptr;
+    Nodo* head = nullptr;
 
-    // Insert some elements into the linked list
-    insert(head, 1);
-    insert(head, 2);
-    insert(head, 3);
-    insert(head, 4);
-    insert(head, 5);
-
-    // Print the linked list
+    insertar(head, 1);
+    insertar(head, 2);
+    insertar(head, 3);
+    insertar(head, 4);
+    insertar(head, 5);
     std::cout << "Linked List: \n";
-    printList(head);
-
-    // Delete the linked list
-    deleteList(head);
+    imprimir(head);
 
     return 0;
 }
