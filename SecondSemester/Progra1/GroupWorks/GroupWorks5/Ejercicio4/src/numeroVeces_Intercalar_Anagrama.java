1f8026cc38e29f88b9372042c65f7c2d45698646
MrRobotAE10 <sucoandchino@gmail.com>
2024-04-14 17:45:39 -0500

    Finish exercise 4

--- SecondSemester/Progra1/GroupWorks/GroupWorks5/Ejercicio4/src/numeroVeces_Intercalar_Anagrama.java
+++ SecondSemester/Progra1/GroupWorks/GroupWorks5/Ejercicio4/src/numeroVeces_Intercalar_Anagrama.java
@@ -4,8 +4,8 @@
  */
 import java.util.Scanner;
 public class numeroVeces_Intercalar_Anagrama {
+	// Devuelve una cadena en minusculas y sin espacios en blanco
 	public static String convertirCadena(String cadena){
-		// Devuelve una cadena en minusculas y sin espacios en blanco
 		cadena = cadena.toLowerCase();
 		return cadena.trim();
 	}
@@ -17,10 +17,11 @@ public class numeroVeces_Intercalar_Anagrama {
 		return convertirCadena(cadena);
 	}
 	
+	// Metodo para el literal a
 	public static int numeroVeces(String cadena1, String cadena2){
 		int repetidos = 0;
 		int index = 0;
-		// Mientras se encuentre la cadena 2 en la cadena 1
+		// Mientras se encuentre una ocurrencia
 		while((index = cadena1.indexOf(cadena2, index)) != -1){
 			repetidos++;
 			// Mueve el index para que compare despues de una ocurrencia
@@ -29,10 +30,49 @@ public class numeroVeces_Intercalar_Anagrama {
 		return repetidos;
 	}
 
+	// Metodo para el literal b
+	public static String intercalar(String cadena1, String cadena2){
+		String cadena = " ";
+		// Si los tamanos de las cadenas son distintos itera hasta el tamano de la cadena mas pequena
+		int minimo = Math.min(cadena1.length(), cadena2.length());
+		for(int i = 0; i < minimo; i++){
+			cadena += cadena1.charAt(i);
+			cadena += cadena2.charAt(i);
+		}
+		
+		// Substring devuelve una cadena vacia si el indice de inicio es igual al tamano de la cadena
+		// Devuelve la cadena mas los caracteres restantes de la cadena mas larga
+		return cadena + cadena1.substring(minimo) + cadena2.substring(minimo);
+	}
+
+	// Metodo para el literal c
+	public static boolean esAnagrama(String cadena1, String cadena2){
+		if(cadena1.length() != cadena2.length()){
+			return false;
+		}
+		// Array para cada letra del abecedario en espanol
+		int contador[] = new int[27];
+
+		for(int i = 0; i < cadena1.length(); i++){
+			contador[cadena1.charAt(i) - 'a']++; // - 'a' convierte el caracter en la posicion i a su valor numerico UNICODE 
+		}
+		for(int i = 0; i < cadena2.length(); i++){
+			contador[cadena2.charAt(i) - 'a']--;
+		}
+
+		for(int cont : contador){
+			if(cont != 0){
+				return false;
+			}
+		}
+
+		return true;
+	}
+
 
     public static void main(String[] args) {
-		Scanner entrada = new Scanner(System.in, "ISO-8859-1");
 		Scanner caracter = new Scanner(System.in);
+		String cadena1, cadena2;
 		char opcion;
 		do {
 			System.out.println("Menu");
@@ -46,13 +86,23 @@ public class numeroVeces_Intercalar_Anagrama {
 			opcion = Character.toLowerCase(opcion);
 			switch(opcion){
 				case 'a':
-					String cadena1 = pedirCadena(1);
-					String cadena2 = pedirCadena(2);
-					System.out.println("La subcadena " + cadena2 + " aparece " + numeroVeces(cadena1, cadena2) + " en la cadena " + cadena1);
+					cadena1 = pedirCadena(1);
+					cadena2 = pedirCadena(2);
+					System.out.println("La subcadena " + cadena2 + " aparece " + numeroVeces(cadena1, cadena2) + " veces en la cadena " + cadena1);
 					break;
 				case 'b':
+					cadena1 = pedirCadena(1);
+					cadena2 = pedirCadena(2);
+					System.out.println("Resultado " + intercalar(cadena1, cadena2));
 					break;
 				case 'c':
+					cadena1 = pedirCadena(1);
+					cadena2 = pedirCadena(2);
+					if(esAnagrama(cadena1, cadena2)){
+						System.out.println("Las cadenas son anagramas");	
+					} else{
+						System.out.println("Las cadenas no son anagramas");
+					}
 					break;
 				case 'd':
 					System.out.println("Fin del programa");
